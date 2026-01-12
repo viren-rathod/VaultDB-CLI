98d218c9f7ec2e8b5d465f76bdcb86f9b062e37d
viren.rathod <viren.rathod.2023@gmail.com>
2026-01-12 14:09:12 +0530

    Initial project setup

---
+++ src/main/java/com/vrn/backup/vaultdb_cli/VaultDbCliApplication.java
@@ -0,0 +1,13 @@
+package com.vrn.backup.vaultdb_cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultDbCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultDbCliApplication.class, args);
+	}
+
+}
